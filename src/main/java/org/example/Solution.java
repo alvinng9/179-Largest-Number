c32e571a59faf7ca710b3d263f2adf89c6c7accb
Alvin <alvinngg9@gmail.com>
2024-08-22 18:22:32 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,19 +4,26 @@ import java.util.Arrays;
 
 public class Solution {
     public String largestNumber(int[] nums) {
-        String[] result = new String[nums.length];
+        //convert the int array to a string array
+        String[] strings = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            result[i] = String.valueOf(nums[i]);
+            strings[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(result, (a, b) -> {
+
+        //sort the new string array in a way where the combination of
+        //2 numbers would produce the largest number
+        Arrays.sort(strings, (a, b) -> {
             return (b + a).compareTo(a + b);
         });
-        if (result[0].equals("0")) {
+
+        //check for the case where the int array only has 0
+        if (strings[0].equals("0")) {
             return "0";
+            //add all the values and return
         } else {
             StringBuilder sb = new StringBuilder();
-            for (String here : result) {
-                sb.append(here);
+            for (String string : strings) {
+                sb.append(string);
             }
             return sb.toString();
         }
